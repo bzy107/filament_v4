5ad7a3a887f8173922633018715ed136982def69
bzy107 <bzy107>
2025-08-11 21:26:14 +0900

    feat: modify owner resource

--- app/Filament/Resources/OwnerResource.php
+++ app/Filament/Resources/OwnerResource.php
@@ -50,14 +50,16 @@ class OwnerResource extends Resource
                     ->where('patients.registered_at', $maxDate);
             })
             ->columns([
-                TextColumn::make('type'),
-                TextColumn::make('email'),
-                TextColumn::make('phone'),
+                TextColumn::make('email')
+                    ->label('owner email'),
+                TextColumn::make('phone')
+                    ->label('owner phone'),
                 TextColumn::make('owner_name'),
                 TextColumn::make('patient_name'),
-                TextColumn::make('patient_name'),
+                TextColumn::make('type')
+                    ->label('patient type'),
                 TextColumn::make('registered_at')
-                    ->label('registered_at')
+                    ->label('patient registered_at')
                     ->formatStateUsing(
                         function ($record) {
                             $date = Carbon::parse($record->registered_at);
@@ -67,27 +69,29 @@ class OwnerResource extends Resource
                         }
                     ),
                 TextColumn::make('breed_name'),
-                TextColumn::make('species')
-                    ->sortable()
-                    ->formatStateUsing(
-                        function ($record) {
-                            $breeds = '';
-                            if (preg_match('/Dog/', $record->species)) {
-                                $breeds = 'Dog ';
-                            }
-                            if (preg_match('/Rabbit/', $record->species)) {
-                                $breeds = 'Rabbit ';
-                            }
-                            if (preg_match('/Cat/', $record->species)) {
-                                $breeds = 'Cat ';
-                            }
-                            if (preg_match('/Bird/', $record->species)) {
-                                $breeds = 'Bird ';
-                            }
-                            return $breeds . $record->breed_id;
-                        }
-                    ),
-                TextColumn::make('note'),
+                // TextColumn::make('species')
+                //     ->label('breed species')
+                //     ->sortable()
+                //     ->formatStateUsing(
+                //         function ($record) {
+                //             $breeds = '';
+                //             if (preg_match('/Dog/', $record->species)) {
+                //                 $breeds = 'Dog ';
+                //             }
+                //             if (preg_match('/Rabbit/', $record->species)) {
+                //                 $breeds = 'Rabbit ';
+                //             }
+                //             if (preg_match('/Cat/', $record->species)) {
+                //                 $breeds = 'Cat ';
+                //             }
+                //             if (preg_match('/Bird/', $record->species)) {
+                //                 $breeds = 'Bird ';
+                //             }
+                //             return $breeds . $record->breed_id;
+                //         }
+                //     ),
+                // TextColumn::make('note')
+                //     ->label('breed note'),
                 TextColumn::make('vaccine_name'),
             ])
             ->filters([
